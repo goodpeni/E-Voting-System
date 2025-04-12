8b870563281dcb25416725e1570a5bf4b1a02d24
weimodein <jhorentuazon.dev@gmail.com>
2025-04-12 10:08:49 +0800

    Added SQL on login form of Admin(Form3)

--- Form3.cs
+++ Form3.cs
@@ -7,14 +7,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace E_Voting_System
 {
     public partial class Form3 : Form
     {
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=EVotingDB";
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        public void Login()
+        {
+            string query = "SELECT * FROM admin_info WHERE admin_id = '" + txtAdminID.Text + "'AND admin_password = '" + txtAdminPass.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        MessageBox.Show("Login Success!");
+                        Form7 Form7 = new Form7();
+                        Form7.Show();
+                        this.Hide();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Oops! Something went wrong. Please try again!");
+                    txtAdminID.Clear();
+                    txtAdminPass.Clear();
+                    chkShowPass.Checked = false;
+                }
+                databaseConnection.Close(); 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnGoBack_Click(object sender, EventArgs e)
@@ -38,19 +79,8 @@ namespace E_Voting_System
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if (txtAdminID.Text == "root" && txtAdminPass.Text == "admin")
-            {
-                Form7 Form7 = new Form7();
-                Form7.Show();
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Admin ID or Password is Incorrect!");
-                txtAdminID.Clear();
-                txtAdminPass.Clear();
-                chkShowPass.Checked = false;
-            }
+            Login();          
         }
     }
 }
+
